79429bf7c12459f4d53b8e0fecfcfc2628a3a491
Md Asfikur Rahman Sakib <arsakib299@gmail.com>
2026-05-07 08:47:42 +0600

    Solved M_Capital_or_Small_or_Digit.cpp

---
+++ Codeforces/M_Capital_or_Small_or_Digit.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char x;
+    cin>>x;
+   if(x>=48 && x<=57)
+    cout<<"IS DIGIT"<<endl;
+    else if(x>='A' &&  x<='Z')
+    cout<<"ALPHA"<<endl<<"IS CAPITAL"<<endl;
+    else if(x>='a' &&  x<='z')
+    cout<<"ALPHA"<<endl<<"IS SMALL"<<endl;
+ 
+}
