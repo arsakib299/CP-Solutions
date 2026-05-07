dadf22820b19ef336ad90003d9ba5e9f97acbb64
Md Asfikur Rahman Sakib <arsakib299@gmail.com>
2026-05-07 21:38:22 +0600

    Restructure Beecrowd folders

---
+++ Beecrowd/DIU_PPS_GREEN_SHEET/Output_2.c
@@ -0,0 +1,18 @@
+// problem link:https://judge.beecrowd.com/en/problems/view/2748
+// Author: arsakib299
+
+#include <stdio.h>
+
+int main()
+{
+printf("---------------------------------------\n");
+printf("|        Roberto                      |\n");
+printf("|                                     |\n");
+printf("|        5786                         |\n");
+printf("|                                     |\n");
+printf("|        UNIFEI                       |\n");
+printf("---------------------------------------\n");
+
+
+    return 0;
+}
