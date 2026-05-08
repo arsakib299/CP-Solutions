fac64ec6f53278a42f8cfaa4b878b27dae36540e
Md Asfikur Rahman Sakib <arsakib299@gmail.com>
2026-05-08 09:15:47 +0600

    Solved: Extremely_Basic.c

---
+++ Beecrowd/DIU_PPS_GREEN_SHEET/Extremely_Basic.c
@@ -0,0 +1,13 @@
+// problem link:https://judge.beecrowd.com/en/problems/view/1001
+// Author: arsakib299
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("X = %d\n",a+b);
+
+    return 0;
+}
