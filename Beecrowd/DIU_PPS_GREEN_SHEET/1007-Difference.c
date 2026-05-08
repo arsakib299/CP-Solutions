357fac8402c2e776470e14621b98ac2fbf0c9c4e
Md Asfikur Rahman Sakib <arsakib299@gmail.com>
2026-05-08 09:23:04 +0600

    Solved: 1007-Difference.c

---
+++ Beecrowd/DIU_PPS_GREEN_SHEET/1007-Difference.c
@@ -0,0 +1,15 @@
+// problem link:https://judge.beecrowd.com/en/problems/view/1007
+// Author: arsakib299
+
+#include <stdio.h>
+
+int main()
+{
+    
+ int A,B,C,D;
+ scanf("%d %d %d %d",&A,&B,&C,&D);
+ int result=(A*B-C*D);
+ printf("DIFERENCA = %d\n",result);
+
+    return 0;
+}
