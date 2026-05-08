d9f6f2314f8ca7ea00d768338cc1f3663ce8bdd8
Md Asfikur Rahman Sakib <arsakib299@gmail.com>
2026-05-08 16:24:44 +0600

    Solved: H_Sorting.c

---
+++ Codeforces/H_Sorting.c
@@ -0,0 +1,36 @@
+// problem link:https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/H
+// Author: arsakib299
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                //swap
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    return 0;
+}
