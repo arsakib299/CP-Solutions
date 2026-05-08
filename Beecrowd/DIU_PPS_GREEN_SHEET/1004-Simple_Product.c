8ebf4022c384809e9493c916a92dd5426513b8f6
Md Asfikur Rahman Sakib <arsakib299@gmail.com>
2026-05-08 09:19:55 +0600

    Solved: 1004-Simple_Product.c

---
+++ Beecrowd/DIU_PPS_GREEN_SHEET/1004-Simple_Product.c
@@ -0,0 +1,14 @@
+// problem link:https://judge.beecrowd.com/en/problems/view/1004
+// Author: arsakib299
+
+#include <stdio.h>
+
+int main()
+{
+   int x,y,PROD;
+   scanf("%d %d",&x,&y);
+   PROD=x*y;
+   printf("PROD = %d\n",PROD);
+
+   return 0;
+}
