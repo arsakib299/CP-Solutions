6baa3b90914cdaadcd72a3202b237205e6e0a262
Md Asfikur Rahman Sakib <arsakib299@gmail.com>
2026-05-15 20:05:32 +0600

    Solved: A_Domino_piling.c

---
+++ Codeforces/90Days-CF-Camp-ShohojCoding/Day-2/A_Domino_piling.c
@@ -0,0 +1,15 @@
+// problem link:https://codeforces.com/problemset/problem/50/A
+// Author: arsakib299
+
+#include <stdio.h>
+
+int main()
+{
+    int m,n;
+    scanf("%d %d",&m,&n);
+
+    int result=(m*n)/2;
+    printf("%d",result);
+
+    return 0;
+}
