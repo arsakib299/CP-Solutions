99b8b26a8e0b267c323f042be7f304af173349cf
Md Asfikur Rahman Sakib <arsakib299@gmail.com>
2026-05-18 21:22:55 +0600

    Solved: A_Beautiful_Year.cpp

---
+++ Codeforces/90Days-CF-Camp-ShohojCoding/Day-5/A_Beautiful_Year.cpp
@@ -0,0 +1,33 @@
+// problem link:https://codeforces.com/problemset/problem/271/A
+// Author: arsakib299
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int year;
+
+    cin>>year;
+
+    while(1)
+    {
+        year++;
+
+        int a=year/1000;
+        int b=(year/100)%10;
+        int c=(year/10)%10;
+        int d=year%10;
+
+        if(a!=b && a!=c && a!=d &&
+           b!=c && b!=d &&
+           c!=d)
+        {
+            cout<<year;
+
+            break;
+        }
+    }
+
+    return 0;
+}
